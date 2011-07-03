c3718efa8fe0afd56d9657fa18c2532ea4060251
Víctor Berga <vbergae@gmail.com>
2011-07-03 18:25:38 +0200

    Added json response

--- Controller/TaskController.php
+++ Controller/TaskController.php
@@ -3,6 +3,7 @@
 namespace VBcom\TaskServerBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
 
 use VBcom\TaskServerBundle\Entity\Task;
 use VBcom\TaskServerBundle\Form\TaskType;
@@ -14,25 +15,22 @@ use VBcom\TaskServerBundle\Form\TaskType;
 class TaskController extends Controller
 {
     /**
-     * Lists all Task entities.
-     *
+     * Find all tasks
+     * @return type 
      */
-    public function indexAction()
+    private function findAll()
     {
         $em = $this->getDoctrine()->getEntityManager();
 
         $entities = $em->getRepository('VBcomTaskServerBundle:Task')->findAll();
-
-        return $this->render('VBcomTaskServerBundle:Task:index.html.twig', array(
-            'entities' => $entities
-        ));
+        
+        return $entities;
     }
-
-    /**
-     * Finds and displays a Task entity.
-     *
+  
+    /** 
+     * Return 1 task entity
      */
-    public function showAction($id)
+    private function show($id)
     {
         $em = $this->getDoctrine()->getEntityManager();
 
@@ -41,7 +39,70 @@ class TaskController extends Controller
         if (!$entity) {
             throw $this->createNotFoundException('Unable to find Task entity.');
         }
+        
+        return $entity;
+    }
+    
+    /**
+     * Converts $entity object to array
+     * 
+     * @param type $entity
+     * @return mixed
+     */
+    private function entityToArray($entity)
+    {
+        return array(
+          'id'          => $entity->getId(),
+          'title'       => $entity->getTitle(),
+          'description' => $entity->getDescription(),
+          'start_date'  => $entity->getStartDate(),
+          'finish_date' => $entity->getFinishDate(),
+          'created_at'  => $entity->getCreatedAt(),
+          'updated_at'  => $entity->getUpdatedAt(),
+          'done'        => $entity->getDone(),
+          'priority'    => $entity->getPriority()
+        );
+    }
+    
+    /**
+     * Lists all Task entities.
+     *
+     */
+    public function indexAction()
+    {
+        $entities = $this->findAll();
+        return $this->render('VBcomTaskServerBundle:Task:index.html.twig', array(
+            'entities' => $entities
+        ));
+    }
 
+    /**
+     * Lists all Task entities in json format.
+     *
+     */    
+    public function indexJsonAction()
+    {
+        $entities = $this->findAll();
+        $tasks = array();
+        foreach ($entities as $entity)
+        {
+          $task = $this->entityToArray($entity);
+          $tasks[] = $task;
+        }
+
+        $response = new Response(json_encode($tasks));
+        $response->headers->set('Content-Type', 'application/json');
+
+        return $response;
+    }
+    
+    /**
+     * Finds and displays a Task entity.
+     *
+     */
+    public function showAction($id)
+    {
+        $entity = $this->show($id);
         $deleteForm = $this->createDeleteForm($id);
 
         return $this->render('VBcomTaskServerBundle:Task:show.html.twig', array(
@@ -49,6 +110,21 @@ class TaskController extends Controller
             'delete_form' => $deleteForm->createView(),
         ));
     }
+
+    /**
+     * Finds and displays a Task entity in json format.
+     *
+     */    
+    public function  showJsonAction($id)
+    {
+        $task = $this->entityToArray($this->show($id));
+        
+        $response = new Response(json_encode($task));
+        $response->headers->set('Content-Type', 'application/json');
+        
+        return $response;
+    }
+
 
     /**
      * Displays a form to create a new Task entity.
